cb999d55a3b4e8fa575f8d862ba6c7f51caebea9
David Reiss <reiss@uw.edu>
2011-03-25 17:11:36 -0700

    Updated to version 4.8.3 * modified motif annealing schedule to start motifing later and increase frequency towards end * no motifing of biclusters that havent changed since last motifing * improvements to write.bicluster.network() including node images for cytoscape * other relatively minor changes and improvements and bug fixes

--- cmonkey-plotting.R
+++ cmonkey-plotting.R
@@ -397,12 +397,12 @@ plotCluster.network <- function( cluster, network="all", o.genes=NULL, colors=NU
   }
   network <- cluster$network
   nrows <- rows; if ( ! is.null( o.genes ) ) nrows <- c( nrows, o.genes )
-  if ( is.null( cluster$cog.code ) && ! is.null( genome.info$cog.code ) )
+  if ( is.null( cluster$cog.code ) && "cog.code" %in% names( genome.info ) )
     cluster$cog.code <- genome.info$cog.code[ rows ]
   
   if ( is.null( cluster$colors ) ) {
     if ( is.null( colors ) ##&& exists( "gene.coords" )
-        || ! is.null( cluster$cog.code ) ) {
+        || "cog.code" %in% names( genome.info ) ) {
       tmp.lett <- 1:26
       names( tmp.lett ) <- LETTERS
       ##if ( exists( "gene.coords" ) && ! is.vector( gene.coords ) && ! is.null( gene.coords$gene.code ) )
@@ -605,7 +605,7 @@ plotClusterMotifPositions <- function( cluster, seqs=cluster$seqs, long.names=T,
   ##}
 
   if ( is.null( colors ) ##&& exists( "gene.coords" )
-      || ! is.null( cluster$cog.code ) ) {
+      || "cog.code" %in% names( genome.info ) ) {
     tmp.lett <- 1:26
     names( tmp.lett ) <- LETTERS
     ##if ( exists( "gene.coords" ) && ! is.vector( gene.coords ) && ! is.null( gene.coords$gene.code ) )
@@ -903,7 +903,7 @@ plotClust <- function( k, cluster=NULL, w.motifs=T, all.conds=T, title=NULL, o.g
         ##!else if ( "gene" %in% colnames( c[[ st ]]$motif.out$pv.ev[[ 2 ]] ) ) c[[ st ]]$motif.out$pv.ev[[ 2 ]] <- c[[ st ]]$motif.out$pv.ev[[ 2 ]]
         if ( ! is.null( meme.scores[[ st ]]$all.pv ) ) {
           tmp <- cbind( p.value=meme.scores[[ st ]]$all.pv[ ,k ],
-                   e.value=if ( ! is.null( meme.scores[[ st ]]$all.ev ) ) meme.scores[[ st ]]$all.ev[ ,k ] else NA )
+                 e.value=if ( "all.ev" %in% names( meme.scores[[ st ]] ) ) meme.scores[[ st ]]$all.ev[ ,k ] else NA )
         } else {
           pv.ev <- meme.scores[[ st ]][[ k ]]$pv.ev[[ 1 ]]
           if ( ncol( pv.ev ) <= 2 ) pv.ev <- meme.scores[[ st ]][[ k ]]$pv.ev[[ 2 ]]
@@ -935,7 +935,7 @@ plotClust <- function( k, cluster=NULL, w.motifs=T, all.conds=T, title=NULL, o.g
     }
   }
   c$gene.coords <- get.long.names( rows, short=short.names )
-  if ( ! is.null( genome.info$cog.code ) ) c$cog.code <- genome.info$cog.code[ rows ] 
+  if ( "cog.code" %in% names( genome.info ) ) c$cog.code <- genome.info$cog.code[ rows ] 
   if ( ! is.null( title ) ) c$name <- title
   ##cluster<<-c
   if ( ! dont.plot ) {
@@ -1683,7 +1683,10 @@ write.project <- function( ks=sapply( as.list( clusterStack ), "[[", "k" ), ##sa
 
 write.bicluster.network <- function( out.dir=NULL, ks=1:k.clust, seq.type=names( e$mot.weights )[ 1 ], tomtom=T,
                                     tt.filter=function( tt ) subset( tt, overlap >= 4 & q.value <= 0.05 ),
-                                    m.filter=function( m ) subset( m, e.value <= Inf ), ... ) {
+                                    m.filter=function( m ) subset( m, e.value <= Inf ),
+                                    gene.url=function( g ) sprintf( "http://microbesonline.org/cgi-bin/keywordSearch.cgi?taxId=%d&keyword=%s", taxon.id, g ),
+                                    image.urls=T, ## currently ignored
+                                    ... ) {
   if ( is.null( out.dir ) ) {
     out.dir <- paste( cmonkey.filename, "network", sep="/" )
     if ( iter != n.iter ) out.dir <- sprintf( "%s_%04d/network", cmonkey.filename, iter )
@@ -1726,7 +1729,7 @@ write.bicluster.network <- function( out.dir=NULL, ks=1:k.clust, seq.type=names(
   out.sif <- rbind( r.sif[ ,1:3 ], m.sif[ ,1:3 ], tt.sif[ ,1:3 ] )
   m.eda <- data.frame( edge=paste( m.sif[ ,1 ], " (", m.sif[ ,2 ], ") ", m.sif[ ,3 ], sep="" ),
                      m.sif[ ,4:ncol( m.sif ) ] )
-  
+
   node.type <- as.data.frame( rbind( as.matrix( data.frame( attr( ratios, "rnames" ), "gene" ) ),
                                     as.matrix( data.frame( attr( ratios, "cnames" ), "condition" ) ),
                                     as.matrix( data.frame( sprintf( "bicluster_%04d", ks ), "bicluster" ) ),
@@ -1741,14 +1744,22 @@ write.bicluster.network <- function( out.dir=NULL, ks=1:k.clust, seq.type=names(
   s.names <- do.call( rbind, lapply( attr( ratios, "rnames" ),
              function( g ) data.frame( g, paste( get.long.names( g, short=T )[[ g ]], collapse="::" ) ) ) )
   colnames( s.names ) <- c( "gene", "short.name" )
+  g.url <- NULL
+  if ( ! is.null( gene.url ) ) {
+    g.url <- do.call( rbind, lapply( attr( ratios, "rnames" ), function( g ) data.frame( g, gene.url( g ) ) ) )
+    colnames( g.url ) <- c( "gene", "url" )
+  }
   mot.info <- do.call( rbind, lapply( ks, function( k ) if ( length( ms[[ k ]]$meme ) <= 0 ) NULL else
                                      data.frame( sprintf( "motif_%04d_%d", k, 1:length( ms[[ k ]]$meme.out ) ),
                                                 sapply( ms[[ k ]]$meme.out, function( i ) pssm.to.string( i$pssm ) ),
                                                 sapply( ms[[ k ]]$meme.out, "[[", "width" ),
                                                 sapply( ms[[ k ]]$meme.out, "[[", "sites" ),
                                                 sapply( ms[[ k ]]$meme.out, "[[", "llr" ),
-                                                sapply( ms[[ k ]]$meme.out, "[[", "e.value" ) ) ) )
-  colnames( mot.info ) <- c( "motif", "consensus", "width", "n.sites", "llr", "e.value" )
+                                                sapply( ms[[ k ]]$meme.out, "[[", "e.value" ),
+                                                sprintf( "file://%s/%s/htmls/cluster%04d_pssm%d.png",
+                                                        getwd(), out.dir, k, 1:length( ms[[ k ]]$meme.out ) )
+                                                ) ) )
+  colnames( mot.info ) <- c( "motif", "consensus", "width", "n.sites", "llr", "e.value", "imgURL" )
   clust.info <- lapply( c( "resid", "p.clust", "e.val", "nrows", "ncols" ),
                        function( i ) sapply( ks, function( j ) clusterStack[[ j ]][[ i ]] ) )
   names( clust.info ) <- c( "resid", "p.clust", "e.val", "nrows", "ncols" )
@@ -1757,7 +1768,9 @@ write.bicluster.network <- function( out.dir=NULL, ks=1:k.clust, seq.type=names(
   for ( n in names( clust.info ) ) if ( ! is.null( ncol( clust.info[[ n ]] ) ) &&
                                        is.null( colnames( clust.info[[ n ]] ) ) )
     colnames( clust.info[[ n ]] ) <- paste( n, 1:ncol( clust.info[[ n ]] ), sep="." )
-  clust.info <- cbind( bicluster=sprintf( "bicluster_%04d", ks ), do.call( cbind, clust.info ) )
+  clust.info <- cbind( bicluster=sprintf( "bicluster_%04d", ks ), do.call( cbind, clust.info ),
+                      url=sprintf( "file://%s/%s/htmls/cluster%04d.html", getwd(), out.dir, ks ),
+                      imgURL=sprintf( "file://%s/%s/htmls/cluster%04d_profile.png", getwd(), out.dir, ks ) )
   rownames( clust.info ) <- NULL
   ## NOW MERGE ALL NOAs INTO ONE TAB-DELIMITED TABLE FOR LOADING INTO CYTOSCAPE
   noa <- merge( node.type, syn.names, by.x="node", by.y="gene", all=T )
@@ -1765,6 +1778,10 @@ write.bicluster.network <- function( out.dir=NULL, ks=1:k.clust, seq.type=names(
   noa <- merge( noa, s.names, by.x="node", by.y="gene", all=T )
   noa <- merge( noa, mot.info, by.x="node", by.y="motif", all=T )
   noa <- merge( noa, clust.info, by.x="node", by.y="bicluster", all=T )
+  if ( ! is.null( g.url ) ) noa <- merge( noa, g.url, by.x="node", by.y="gene", all=T )
+  noa$imgURL <- as.character( noa$imgURL.x )
+  noa$imgURL[ ! is.na( noa$imgURL.y ) ] <- as.character( noa$imgURL.y[ ! is.na( noa$imgURL.y ) ] )
+  noa <- noa[ , ! colnames( noa ) %in% c( "imgURL.x", "imgURL.y" ) ]
   write.table( out.sif, quote=F, sep="\t", col.names=T, row.names=F, file=paste( out.dir, "all.sif", sep="/" ) )
   ##write.table( c.sif, quote=F, sep="\t", col.names=T, row.names=F, file=paste( out.dir, "c.sif", sep="/" ) )
   write.table( noa, quote=F, sep="\t", col.names=T, row.names=F, file=paste( out.dir, "all.noa", sep="/" ) )
